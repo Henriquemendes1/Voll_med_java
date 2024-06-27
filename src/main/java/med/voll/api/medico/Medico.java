d57b8c8064c45e4c171a69af6a68578c43ed5ec1
Henriquemendes1 <henrique.mend258@gmail.com>
2024-06-27 17:35:56 -0300

    Commit

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -41,4 +41,16 @@ public class Medico {
     }
 
 
+    public void atualizarInformacoes(DadosAtualizacaoMedicoDTO dados) {
+        if(dados.nome() != null){
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null){
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+
+    }
 }
