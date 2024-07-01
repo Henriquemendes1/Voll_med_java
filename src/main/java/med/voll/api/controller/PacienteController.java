6b308ae6e7501dd3e83c357ffd63d06c6589380f
Henriquemendes1 <henrique.mend258@gmail.com>
2024-07-01 12:16:30 -0300

    Add tratamento de Exceptios

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,10 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosAtualizacaoMedicoDTO;
-import med.voll.api.medico.DadosDetalhamentoMedicoDTO;
-import med.voll.api.medico.DadosListagemMedicoDTO;
-import med.voll.api.paciente.*;
+import med.voll.api.domain.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
--- src/main/java/med/voll/api/domain/medico/Medico.java
+++ src/main/java/med/voll/api/domain/medico/Medico.java
@@ -1,52 +1,56 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.medico;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import med.voll.api.endereco.Endereco;
+import med.voll.api.domain.endereco.Endereco;
 
-@Table(name = "pacientes")
-@Entity(name = "Paciente")
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = "id")
-public class Paciente {
+@EqualsAndHashCode(of= "id")
+public class Medico {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
-
     private String telefone;
+    private String crm;
 
-    private String cpf;
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
 
     @Embedded
     private Endereco endereco;
 
     private Boolean ativo;
 
-    public Paciente(DadosCadastroPacienteDTO dados) {
+    public Medico(DadosCadastroMedicoDTO dados) {
         this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
-        this.cpf = dados.cpf();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
     }
 
-    public void atualizarInformacoes(DadosAtualizacaoPacienteDTO dados) {
-        if (dados.nome() != null) {
+
+    public void atualizarInformacoes(DadosAtualizacaoMedicoDTO dados) {
+        if(dados.nome() != null){
             this.nome = dados.nome();
         }
-        if (dados.telefone() != null) {
+        if (dados.telefone() != null){
             this.telefone = dados.telefone();
         }
-        if (dados.endereco() != null) {
+        if (dados.endereco() != null){
             this.endereco.atualizarInformacoes(dados.endereco());
         }
 
